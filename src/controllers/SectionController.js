ec14587132fcf544b33f899444b5c6b334df8239
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2024-05-13 08:42:57 -0300

    New changes

---
+++ src/controllers/SectionController.js
@@ -0,0 +1,16 @@
+/*
+    index (listAll): Listagem de seções;
+    store (add): Criar uma seção;
+    show (list): Quando queremos listar uma ÚNICA seção;
+    update: Quando queremos alterar uma seção;
+    destroy (delete): Quando queremos deletar uma seção
+*/
+
+import User from '../models/User'
+import * as Yup from 'yup'
+
+class SectionController {
+    async store(req, res){
+        
+    }
+}
